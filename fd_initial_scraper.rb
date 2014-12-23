afba2e8b0ff4624d18fda88af10b8feda8559b94
Matt Adams <adams.matt.01@gmail.com>
2014-12-22 19:41:49 -0500

    added flowing_data scripts

---
+++ fd_initial_scraper.rb
@@ -0,0 +1,66 @@
+## script to initially populate mongodb collection with all flowing data posts
+
+require "nokogiri"
+require "open-uri"
+require "mongo"
+
+include Mongo
+
+db = MongoClient.new("192.168.11.3", 27017).db("flowing_data")
+# db = MongoClient.new().db("flowing_data")
+posts = db.collection("posts")
+
+# remove records if any exist
+posts.remove
+
+base_url = "http://flowingdata.com/page/"
+first_page = Nokogiri::HTML(open(base_url + "1"))
+last_pg_num = first_page.css(".first_last_page").text.to_i
+
+# start scraping all the pages
+puts "Seeding MongoDB with FlowingData posts. Please be patient.\n\n"
+
+for pg_num in last_pg_num.downto(1) do
+
+    print "Scraping #{last_pg_num} pages... [ #{(((last_pg_num - pg_num)/last_pg_num.to_f)*100).round(1)}% ]\r"
+
+    page = Nokogiri::HTML(open(base_url + "#{pg_num}"))
+
+    # look at each post
+    page.css("#recent-posts li.archive-post").reverse_each { |post|
+        doc = {}
+        doc[:title] = post.css("h2 a").text.strip
+
+        # link and date
+        post.css("h2 a[rel='bookmark']").each { |link|
+            doc[:link] = link['href']
+
+            date_string = link['href'][/com\/([0-9]*\/[0-9]*\/[0-9]*)/, 1]
+            doc[:date] = Time.strptime(date_string, "%Y/%m/%d").utc
+        }
+
+        # posted to category
+        doc[:posted_to] = []
+        post.css("a[rel='category tag']").each { |pt|
+            doc[:posted_to].push(pt.text)
+        }
+
+        # tags
+        doc[:tags] = []
+        post.css(".meta-bar a[rel='tag']").each { |tag|
+            doc[:tags].push(tag.text)
+        }
+
+        # image
+        post.css(".archive-featured-image img").each { |img|
+            doc[:image] = img['src']
+        }
+
+        # remove tags array from collection if it's empty
+        doc.delete(:tags) if doc[:tags].empty?
+
+        # add doc to posts collection
+        id = posts.insert(doc)
+    }
+
+end
---
+++ fd_updater.rb
@@ -0,0 +1,77 @@
+#!/usr/bin/env ruby
+
+require "nokogiri"
+require "open-uri"
+require "mongo"
+require "date"
+
+include Mongo
+
+
+db_name = "flowing_data"
+coll_name = "posts"
+db = MongoClient.new("192.168.11.3", 27017).db(db_name)
+# db = MongoClient.new().db(db_name)
+posts = db.collection(coll_name)
+
+most_recent_page = "http://flowingdata.com/page/1"
+
+
+# add posts to flowing_data database
+# that occurred after the current most recent post
+most_recent_post = posts.find.sort(_id: :desc).limit(1).to_a[0]
+
+# start scraping
+page = Nokogiri::HTML(open(most_recent_page))
+
+# keep track of the number of new posts
+post_count = 0
+
+page.css("#recent-posts li.archive-post").reverse.each { |post|
+
+    doc = {} # initialize each document
+    doc[:title]= post.css("h2 a").text.strip
+
+    # skip the posts that already exist in the collection
+    next if posts.find(title: "#{doc[:title]}").to_a.length > 0
+
+    # count the number of posts getting added
+    post_count += 1
+
+    # link and date
+    post.css("h2 a[rel='bookmark']").each { |link|
+        doc[:link] = link['href']
+
+        date_string = link['href'][/com\/([0-9]*\/[0-9]*\/[0-9]*)/, 1]
+        doc[:date] = Time.strptime(date_string, "%Y/%m/%d").utc
+    }
+
+    # posted to category
+    doc[:posted_to] = []
+    post.css("a[rel='category tag']").each { |pt|
+        doc[:posted_to].push(pt.text)
+    }
+
+    # tags
+    doc[:tags] = []
+    post.css(".meta-bar a[rel='tag']").each { |tag|
+        doc[:tags].push(tag.text)
+    }
+
+    # image
+    post.css(".archive-featured-image img").each { |img|
+        doc[:image] = img['src']
+    }
+
+    # remove tags array from collection if it's empty
+    doc.delete(:tags) if doc[:tags].empty?
+
+    # add doc to posts collection
+    id = posts.insert(doc)
+
+}
+
+# completion message
+puts post_count > 0 ?
+"#{DateTime.now} => Added #{post_count} new document(s) to the #{coll_name} collection in the #{db_name} database!\n" :
+"#{DateTime.now} => No new posts.\n"
